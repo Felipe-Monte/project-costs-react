8505f13cf0136368d75398d93dd7bb9b95d44b55
Carlos Felipe <carlosfelipemonte1997@outlook.com>
2024-01-15 21:12:23 -0300

    style footer

--- src/App.js
+++ src/App.js
@@ -1,30 +1,32 @@
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 
+import Navbar from './components/layout/Navbar'
+
 import Container from './components/layout/Container'
 
 import Home from './pages/Home'
+import Projects from './pages/Projects'
 import Company from './pages/Company'
 import Contact from './pages/Contact'
 import NewProject from './pages/NewProject'
 
+import Footer from './components/layout/Footer'
+
 function App() {
   return (
     <Router>
-      <ul>
-        <Link to="/">Home</Link>
-        <Link to="/contact">Contato</Link>
-        <Link to="/company">Empresa</Link>
-        <Link to="/newproject">Novo projeto</Link>
-      </ul>
-      <Container customClass="min-height">
+      <Navbar />
+      
+      <Container customClass="min_height">
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/Company" element={<Company />} />
-          <Route path="/Contact" element={<Contact />} />
-          <Route path="/NewProject" element={<NewProject />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/company" element={<Company />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/newproject" element={<NewProject />} />
         </Routes>
       </Container>
-      <p>Footer</p>
+      <Footer />
     </Router>
   )
 }
---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,34 @@
+import { Link } from "react-router-dom"
+import { PiDevToLogo } from "react-icons/pi";
+
+import Container from './Container'
+
+import styles from './Navbar.module.css'
+
+function Navbar() {
+  return (
+    <nav className={styles.navbar}>
+      <Container>
+        <Link>
+          <PiDevToLogo className={styles.logo}/>
+        </Link>
+        <ul className={styles.list}>
+          <li className={styles.item}>
+            <Link to="/">Home</Link>
+          </li>
+          <li className={styles.item}>
+            <Link to="/projects">Projetos</Link>
+          </li>
+          <li className={styles.item}>
+            <Link to="/company">Empresa</Link>
+          </li>
+          <li className={styles.item}>
+            <Link to="/contact">Contato</Link>
+          </li>
+        </ul>
+      </Container>
+    </nav>
+  )
+}
+
+export default Navbar
