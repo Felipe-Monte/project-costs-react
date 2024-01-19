9095c0082cf18f429e3f4a738003a66a216696c9
Carlos Felipe <carlosfelipemonte1997@outlook.com>
2024-01-18 23:22:18 -0300

    continue code

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Navbar from './components/layout/Navbar'
-
 import Container from './components/layout/Container'
-
 import Home from './pages/Home'
 import Projects from './pages/Projects'
 import Company from './pages/Company'
 import Contact from './pages/Contact'
 import NewProject from './pages/NewProject'
-
 import Footer from './components/layout/Footer'
 
 function App() {
   return (
     <Router>
       <Navbar />
-      
+
       <Container customClass="min_height">
         <Routes>
           <Route path="/" element={<Home />} />
