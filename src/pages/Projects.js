480100ca6683ed8778d9491c7aae9efa419d8134
Carlos Felipe <carlosfelipemonte1997@outlook.com>
2024-01-25 22:33:31 -0300

    continue project

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,7 +1,23 @@
+import { useLocation } from "react-router-dom"
 
-function Projects(){
-  return(
-    <h1>Projetos</h1>
+import Message from "../components/layout/Message"
+
+function Projects() {
+
+  const location = useLocation()
+
+  let message = ""
+  if (location.state) {
+    message = location.state.message
+  }
+
+  return (
+    <div>
+      <h1>Meus projetos</h1>
+      {
+        message && <Message msg={message} type="success" />
+      }
+    </div>
   )
 }
 
