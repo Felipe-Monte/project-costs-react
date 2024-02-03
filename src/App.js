c49eee746cc60a11f95dc8d547859f417ec74346
Carlos Felipe <carlosfelipemonte1997@outlook.com>
2024-02-02 21:48:54 -0300

    continue project

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Projects from "./components/pages/Projects";
 import Company from "./components/pages/Company";
 import Contact from "./components/pages/Contact";
 import NewProject from "./components/pages/NewProject";
+import Project from "./components/pages/Project";
 import Footer from "./components/layout/Footer";
 
 function App() {
@@ -15,11 +16,12 @@ function App() {
 
       <Container customClass="min_height">
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route exact path="/" element={<Home />} />
           <Route path="/projects" element={<Projects />} />
           <Route path="/company" element={<Company />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/newproject" element={<NewProject />} />
+          <Route path="/project/:id" element={<Project />} />
         </Routes>
       </Container>
       <Footer />
